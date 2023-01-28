264dbe829df2c630aa283c0284b557494da58782
IKNNS <kunanoon1506@gmail.com>
2023-01-28 21:53:51 +0700

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import {Button, Typography} from '@mui/material';
+import { Stack } from '@mui/system';
+ 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (  
+    <Stack spacing = {2} direction ='column'>
+      
+      <img src = "/transfer.jpg" alt="transfer-money" width="60" height="60" ></img>
+
+      <Button type = "submit" variant = "contained"  margin = '' sx={{backgroundColor: 'grey.700'}}>
+      <Typography  sx={{color: 'secondary.light' }}>  transfer </Typography>
+      </Button>
+
+      <img src = "/withdraw.jpg" alt="transfer-money" width="60" height="60" ></img>
+
+      <Button type = "submit" variant = "contained"  margin =  '' sx={{backgroundColor: 'grey.700'}}>
+      <Typography sx={{color: 'secondary.light' }}>  withdraw </Typography>
+      </Button>
+
+      <img src = "/deposit.png" alt="transfer-money" width="60" height="60" ></img>
+
+      <Button type = "submit" variant = "contained"  margin = '' sx={{backgroundColor: 'grey.700'}}>
+      <Typography sx={{color: 'secondary.light' }}>  deposit </Typography>
+      </Button>
+    </Stack>
   );
 }
 
